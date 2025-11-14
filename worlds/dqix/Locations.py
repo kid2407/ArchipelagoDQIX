237095f3412d78003767f2236f26d08fb19189da
Tobias Franz <cinex.mail@web.de>
2025-11-14 15:35:30 +0100

    * Cleaned up locations * Added missing Fyggs * Added a lot more regions and connections
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- worlds/dqix/Locations.py
+++ worlds/dqix/Locations.py
@@ -4,18 +4,21 @@ from typing import Dict
 
 
 class DQIXLocations:
-    _location_table: Dict[str, int] = {}
+    _location_table_grouped: Dict[str, Dict[str, int]] = {}
 
     def _parse_location_data(self):
         file = pkgutil.get_data(__name__, "data/locations.json").decode("utf-8")
         locations = json.loads(file)
 
-        self._location_table = locations
+        self._location_table_grouped = locations
 
     def _get_location_table(self):
-        if not self._location_table:
+        if not self._location_table_grouped:
             self._parse_location_data()
-        return self._location_table
+        return self._location_table_grouped
 
     def get_locations(self):
-        return self._get_location_table()
+        return {k: v for sub_dict in self._get_location_table().values() for k, v in sub_dict.items()}
+
+    def get_locations_for_group(self, group_name:str):
+        return self._get_location_table()[group_name]
--- worlds/dqix/__init__.py
+++ worlds/dqix/__init__.py
@@ -49,7 +49,7 @@ class DragonQuestIX(World):
 
     def create_item(self, name: str) -> "DQIXItem":
         return DQIXItem(
-            name,
+            "Fygg" if name.startswith("Fygg ") else name,
             ItemClassification.progression if self.item_helper.is_progression(name) else ItemClassification.useful if self.item_helper.is_useful(name) else ItemClassification.filler,
             self.item_name_to_id[name],
             self.player,
@@ -57,53 +57,201 @@ class DragonQuestIX(World):
         )
 
     def create_items(self) -> None:
+        # Generate / Load all progression items
         progression_item_names = {k for k in self.item_name_to_id.keys() if self.item_helper.is_progression(k)}
         items = [self.create_item(name) for name in progression_item_names]
+
+        # Fill up to 90% of the remaining item slots with useful items
         remaining_count_until_almost_full = (round(len(self.location_name_to_id) * 0.9) - len(items))
         items += [self.create_item(useful_item) for useful_item in self.random.choices(population=list(self.item_helper.useful_items.keys()), k=remaining_count_until_almost_full)]
 
+        # remaining 10% are filler, money and gold
         items += [self.create_item(self.get_filler_item_name()) for _ in range(len(self.location_name_to_id) - len(items))]
 
         self.multiworld.itempool += items
 
     def create_regions(self) -> None:
-        main_region = Region(self.origin_region_name, self.player, self.multiworld)
-        main_region.add_locations(self.location_name_to_id, DQIXLocation)
+        region_angel_falls = Region(self.origin_region_name, self.player, self.multiworld)
+        region_angel_falls.add_locations(locations=self.location_helper.get_locations_for_group(region_angel_falls.name), location_type=DQIXLocation)
+        region_hexagon = Region("Hexagon", self.player, self.multiworld)
+        region_hexagon.add_locations(locations=self.location_helper.get_locations_for_group(region_hexagon.name), location_type=DQIXLocation)
 
-        self.multiworld.regions.append(main_region)
+        region_stornway = Region("Stornway", self.player, self.multiworld)
+        region_stornway.add_locations(locations=self.location_helper.get_locations_for_group(region_stornway.name), location_type=DQIXLocation)
 
-        region_stornway = Region("Stornway / Zere", self.player, self.multiworld)
+        region_zere = Region("Zere", self.player, self.multiworld)
+        region_zere.add_locations(locations=self.location_helper.get_locations_for_group(region_zere.name), location_type=DQIXLocation)
+        region_brigadoom = Region("Brigadoom", self.player, self.multiworld)
+        region_brigadoom.add_locations(locations=self.location_helper.get_locations_for_group(region_brigadoom.name), location_type=DQIXLocation)
+
         region_coffinwell = Region("Coffinwell", self.player, self.multiworld)
-        region_alltrades = Region("Alltrades Abbey / Porth Llaffan / Observatory (After Attack)", self.player, self.multiworld)
-        region_bloomingdale_zere = Region("A new continent (Slurry Quay / Dourbridge / Zere Rocks / Bloomingdale)", self.player, self.multiworld)
-        region_ship_places = Region("A new World (Gleeba, Batsureg, Swinedimpels (Wormwood Creek))", self.player, self.multiworld)
-        region_bowhole = Region("Wormwood Creek / Bowhole", self.player, self.multiworld)
-        region_upover = Region("Upover / Magmaroo", self.player, self.multiworld)
+        region_coffinwell.add_locations(locations=self.location_helper.get_locations_for_group(region_coffinwell.name), location_type=DQIXLocation)
+        region_quarantomb = Region("Quarantomb", self.player, self.multiworld)
+        region_quarantomb.add_locations(locations=self.location_helper.get_locations_for_group(region_quarantomb.name), location_type=DQIXLocation)
+
+        region_observatory = Region("Observatory", self.player, self.multiworld)
+        region_observatory.add_locations(locations=self.location_helper.get_locations_for_group(region_observatory.name), location_type=DQIXLocation)
+
+        region_alltrades_abbey = Region("Alltrades Abbey", self.player, self.multiworld)
+        region_alltrades_abbey.add_locations(locations=self.location_helper.get_locations_for_group(region_alltrades_abbey.name), location_type=DQIXLocation)
+        region_tower_of_trades = Region("Tower of Trades", self.player, self.multiworld)
+        region_tower_of_trades.add_locations(locations=self.location_helper.get_locations_for_group(region_tower_of_trades.name), location_type=DQIXLocation)
+
+        region_porth_llaffan = Region("Porth Llaffan", self.player, self.multiworld)
+        region_porth_llaffan.add_locations(locations=self.location_helper.get_locations_for_group(region_porth_llaffan.name), location_type=DQIXLocation)
+        region_tywll_cave = Region("Tywll Cave", self.player, self.multiworld)
+        region_tywll_cave.add_locations(locations=self.location_helper.get_locations_for_group(region_tywll_cave.name), location_type=DQIXLocation)
+
+        region_slurry_quay = Region("Slurry Quay", self.player, self.multiworld)
+        region_slurry_quay.add_locations(locations=self.location_helper.get_locations_for_group(region_slurry_quay.name), location_type=DQIXLocation)
+
+        region_dourbridge = Region("Dourbridge", self.player, self.multiworld)
+        region_dourbridge.add_locations(locations=self.location_helper.get_locations_for_group(region_dourbridge.name), location_type=DQIXLocation)
+
+        region_heights_of_loneliness = Region("Heights of Loneliness", self.player, self.multiworld)
+        region_heights_of_loneliness.add_locations(locations=self.location_helper.get_locations_for_group(region_heights_of_loneliness.name), location_type=DQIXLocation)
+        region_zere_rocks = Region("Zere Rocks", self.player, self.multiworld)
+        region_zere_rocks.add_locations(locations=self.location_helper.get_locations_for_group(region_zere_rocks.name), location_type=DQIXLocation)
+
+        region_bloomingdale = Region("Bloomingdale", self.player, self.multiworld)
+        region_bloomingdale.add_locations(locations=self.location_helper.get_locations_for_group(region_bloomingdale.name), location_type=DQIXLocation)
+        region_bad_cave = Region("Bad Cave", self.player, self.multiworld)
+        region_bad_cave.add_locations(locations=self.location_helper.get_locations_for_group(region_bad_cave.name), location_type=DQIXLocation)
+
+        region_ocean = Region("Ocean", self.player, self.multiworld)
+        region_ocean.add_locations(locations=self.location_helper.get_locations_for_group(region_ocean.name), location_type=DQIXLocation)
+        region_ship = Region("Ship", self.player, self.multiworld)  # Rooms inside, includes Falcon blade behind ultimate key
+        region_ship.add_locations(locations=self.location_helper.get_locations_for_group(region_ship.name), location_type=DQIXLocation)
+
+        region_gleeba = Region("Gleeba", self.player, self.multiworld)
+        region_gleeba.add_locations(locations=self.location_helper.get_locations_for_group(region_gleeba.name), location_type=DQIXLocation)
+        region_plumbed_depths = Region("Plumbed Depths", self.player, self.multiworld)
+        region_plumbed_depths.add_locations(locations=self.location_helper.get_locations_for_group(region_plumbed_depths.name), location_type=DQIXLocation)
+
+        region_batsureg = Region("Batsureg", self.player, self.multiworld)
+        region_batsureg.add_locations(locations=self.location_helper.get_locations_for_group(region_batsureg.name), location_type=DQIXLocation)
+        region_gerzuun = Region("Gerzuun", self.player, self.multiworld)
+        region_gerzuun.add_locations(locations=self.location_helper.get_locations_for_group(region_gerzuun.name), location_type=DQIXLocation)
+
+        region_swinedimpels = Region("Swinedimpels Academy", self.player, self.multiworld)
+        region_swinedimpels.add_locations(locations=self.location_helper.get_locations_for_group(region_swinedimpels.name), location_type=DQIXLocation)
+        region_old_school = Region("Old School", self.player, self.multiworld)
+        region_old_school.add_locations(locations=self.location_helper.get_locations_for_group(region_old_school.name), location_type=DQIXLocation)
+
+        region_wormwood_creek = Region("Wormwood Creek", self.player, self.multiworld)
+        region_wormwood_creek.add_locations(locations=self.location_helper.get_locations_for_group(region_wormwood_creek.name), location_type=DQIXLocation)
+        region_bowhole = Region("Bowhole", self.player, self.multiworld)
+        region_bowhole.add_locations(locations=self.location_helper.get_locations_for_group(region_bowhole.name), location_type=DQIXLocation)
+
+        region_upover = Region("Upover", self.player, self.multiworld)
+        region_upover.add_locations(locations=self.location_helper.get_locations_for_group(region_upover.name), location_type=DQIXLocation)
+        region_magmaroo = Region("Magmaroo", self.player, self.multiworld)
+        region_magmaroo.add_locations(locations=self.location_helper.get_locations_for_group(region_magmaroo.name), location_type=DQIXLocation)
+
         region_goretress = Region("Goretress", self.player, self.multiworld)
+        region_goretress.add_locations(locations=self.location_helper.get_locations_for_group(region_goretress.name), location_type=DQIXLocation)
+
         region_gittingham_palace = Region("Gittingham Palace", self.player, self.multiworld)
-        region_realm_mighty = Region("Realm of the Mighty", self.player, self.multiworld)
+        region_gittingham_palace.add_locations(locations=self.location_helper.get_locations_for_group(region_gittingham_palace.name), location_type=DQIXLocation)
+        region_oubliette = Region("Oubliette", self.player, self.multiworld)
+        region_oubliette.add_locations(locations=self.location_helper.get_locations_for_group(region_oubliette.name), location_type=DQIXLocation)
 
-        main_region.connect(connecting_region=region_stornway, rule=lambda state: state.has(item="Inny", player=self.player, count=1))
-        region_stornway.connect(connecting_region=region_coffinwell, )  # Check for game chapter
-        region_coffinwell.connect(connecting_region=region_alltrades, )  # Has returned to the Observatory, perhaps count benevolessence?
-        region_alltrades.connect(connecting_region=region_bloomingdale_zere, rule=lambda state: state.has(item="Fygg", player=self.player, count=1))
-        region_bloomingdale_zere.connect(connecting_region=region_ship_places, )  # Check if has unlocked ship
-        region_ship_places.connect(connecting_region=region_bowhole, rule=lambda state: state.has(item="Serene necklace", player=self.player, count=1))  # Bowhole has been unlocked / Seren Necklace?
-        region_bowhole.connect(connecting_region=region_upover, rule=lambda state: state.has(item="Wyrmlight bow", player=self.player, count=1))
-        region_upover.connect(connecting_region=region_goretress, )  # When battle of Greygnarl vs Barbarus happens
+        region_realm_of_the_mighty = Region("Realm of the Mighty", self.player, self.multiworld)
+        region_realm_of_the_mighty.add_locations(locations=self.location_helper.get_locations_for_group(region_realm_of_the_mighty.name), location_type=DQIXLocation)
+
+        region_angel_falls.connect(connecting_region=region_hexagon)
+        region_angel_falls.connect(connecting_region=region_stornway, rule=lambda state: state.has(item="Inny", player=self.player, count=1))  # Maybe magic beast hide from the boss?
+
+        region_stornway.connect(connecting_region=region_zere)
+        region_stornway.connect(connecting_region=region_brigadoom)
+        region_zere.connect(connecting_region=region_brigadoom)
+        region_stornway.connect(connecting_region=region_coffinwell)
+
+        region_coffinwell.connect(connecting_region=region_quarantomb, rule=lambda state: state.has(item="Quarantomb key", player=self.player))
+        region_coffinwell.connect(connecting_region=region_alltrades_abbey)  # Has returned to the Observatory, perhaps count benevolessence?
+        region_coffinwell.connect(connecting_region=region_porth_llaffan)  # Has returned to the Observatory, perhaps count benevolessence?
+        region_coffinwell.connect(connecting_region=region_observatory)  # Has returned to the Observatory, perhaps count benevolessence?
+
+        region_observatory.connect(connecting_region=region_alltrades_abbey)
+        region_observatory.connect(connecting_region=region_porth_llaffan)
+
+        region_alltrades_abbey.connect(connecting_region=region_tower_of_trades)
+        region_alltrades_abbey.connect(connecting_region=region_porth_llaffan)
+
+        region_porth_llaffan.connect(connecting_region=region_slurry_quay, rule=lambda state: state.has(item="Fygg", player=self.player, count=1))
+        region_porth_llaffan.connect(connecting_region=region_tywll_cave)
+
+        region_slurry_quay.connect(connecting_region=region_dourbridge)
+
+        region_dourbridge.connect(connecting_region=region_heights_of_loneliness)
+        region_dourbridge.connect(connecting_region=region_bloomingdale)
+        region_dourbridge.connect(connecting_region=region_bad_cave)
+
+        region_heights_of_loneliness.connect(connecting_region=region_zere_rocks)
+        region_heights_of_loneliness.connect(connecting_region=region_bloomingdale)
+        region_heights_of_loneliness.connect(connecting_region=region_bad_cave)
+
+        region_bloomingdale.connect(connecting_region=region_ocean)  # Check if player has unlocked ship
+
+        region_ocean.connect(connecting_region=region_gleeba)
+        region_ocean.connect(connecting_region=region_batsureg)
+        region_ocean.connect(connecting_region=region_swinedimpels)
+        region_ocean.connect(connecting_region=region_wormwood_creek)
+        region_ocean.connect(connecting_region=region_ship, rule=lambda state: state.has(item="Ultimate key", player=self.player))
+
+        region_gleeba.connect(connecting_region=region_plumbed_depths)
+
+        region_batsureg.connect(connecting_region=region_gerzuun)
+
+        region_swinedimpels.connect(connecting_region=region_old_school)
+
+        region_wormwood_creek.connect(connecting_region=region_bowhole, rule=lambda state: state.has(item="Serene necklace", player=self.player))
+
+        region_bowhole.connect(connecting_region=region_upover, rule=lambda state: state.has(item="Wyrmlight bow", player=self.player))
+
+        region_upover.connect(connecting_region=region_magmaroo)
+        region_upover.connect(connecting_region=region_goretress, rule=lambda state: state.has(item="Drunken Dragon", player=self.player))
+
         region_goretress.connect(connecting_region=region_gittingham_palace, rule=lambda state: state.has(item="Ultimate key", player=self.player, count=1))  # After boss has been defeated
-        region_gittingham_palace.connect(connecting_region=region_realm_mighty, )  # After "killed" by Corvus, perhaps game chapter
 
+        region_gittingham_palace.connect(connecting_region=region_oubliette)
+        region_oubliette.connect(connecting_region=region_realm_of_the_mighty)  # After "killed" by Corvus, perhaps game chapter
+
+        self.multiworld.regions.append(region_angel_falls)
+        self.multiworld.regions.append(region_hexagon)
         self.multiworld.regions.append(region_stornway)
+        self.multiworld.regions.append(region_zere)
+        self.multiworld.regions.append(region_brigadoom)
         self.multiworld.regions.append(region_coffinwell)
-        self.multiworld.regions.append(region_alltrades)
-        self.multiworld.regions.append(region_bloomingdale_zere)
-        self.multiworld.regions.append(region_ship_places)
+        self.multiworld.regions.append(region_quarantomb)
+        self.multiworld.regions.append(region_alltrades_abbey)
+        self.multiworld.regions.append(region_tower_of_trades)
+        self.multiworld.regions.append(region_porth_llaffan)
+        self.multiworld.regions.append(region_tywll_cave)
+        self.multiworld.regions.append(region_slurry_quay)
+        self.multiworld.regions.append(region_dourbridge)
+        self.multiworld.regions.append(region_heights_of_loneliness)
+        self.multiworld.regions.append(region_zere_rocks)
+        self.multiworld.regions.append(region_bloomingdale)
+        self.multiworld.regions.append(region_bad_cave)
+        self.multiworld.regions.append(region_ocean)
+        self.multiworld.regions.append(region_ship)
+        self.multiworld.regions.append(region_gleeba)
+        self.multiworld.regions.append(region_plumbed_depths)
+        self.multiworld.regions.append(region_batsureg)
+        self.multiworld.regions.append(region_gerzuun)
+        self.multiworld.regions.append(region_swinedimpels)
+        self.multiworld.regions.append(region_old_school)
+        self.multiworld.regions.append(region_wormwood_creek)
         self.multiworld.regions.append(region_bowhole)
         self.multiworld.regions.append(region_upover)
+        self.multiworld.regions.append(region_magmaroo)
         self.multiworld.regions.append(region_goretress)
         self.multiworld.regions.append(region_gittingham_palace)
-        self.multiworld.regions.append(region_realm_mighty)
+        self.multiworld.regions.append(region_oubliette)
+        self.multiworld.regions.append(region_realm_of_the_mighty)
+
+        # TODO place event items / locations, where no actual item could be used
 
     def get_filler_item_name(self) -> str:
         return self.random.choice(self.item_helper.get_filler_item_names())
